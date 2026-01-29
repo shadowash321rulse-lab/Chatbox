b823ee10d7107f8a2e4432fbe790c5bc0ed16b48
shadowash321rulse-lab <shadowash321rulse@gmail.com>
2026-01-29 18:31:25 +0000

    Create NowPlayingState.kt

---
+++ app/src/main/kotlin/com/scrapw/chatbox/NowPlayingState.kt
@@ -0,0 +1,26 @@
+package com.scrapw.chatbox.nowplaying
+
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+data class NowPlaying(
+    val isPlaying: Boolean = false,
+    val artist: String = "",
+    val title: String = "",
+    val positionMs: Long = 0L,
+    val durationMs: Long = 1L
+)
+
+object NowPlayingState {
+    private val _state = MutableStateFlow(NowPlaying())
+    val state: StateFlow<NowPlaying> = _state
+
+    fun update(np: NowPlaying) {
+        _state.value = np
+    }
+
+    fun clear() {
+        _state.value = NowPlaying()
+    }
+}
+
