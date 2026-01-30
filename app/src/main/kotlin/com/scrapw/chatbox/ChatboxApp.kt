74b4b5b739a2065bffbcc498985248714ebdff8a
shadowash321rulse-lab <shadowash321rulse@gmail.com>
2026-01-30 03:53:59 +0000

    Update ChatboxApp.kt

--- app/src/main/kotlin/com/scrapw/chatbox/ChatboxApp.kt
+++ app/src/main/kotlin/com/scrapw/chatbox/ChatboxApp.kt
@@ -2,16 +2,11 @@ package com.scrapw.chatbox
 
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavHostController
 import com.scrapw.chatbox.ui.ChatboxViewModel
 
 @Composable
 fun ChatboxApp(
-    navController: NavHostController? = null,
     chatboxViewModel: ChatboxViewModel = viewModel(factory = ChatboxViewModel.Factory)
 ) {
-    ChatboxScreen(
-        navController = navController,
-        chatboxViewModel = chatboxViewModel
-    )
+    ChatboxScreen(chatboxViewModel = chatboxViewModel)
 }
